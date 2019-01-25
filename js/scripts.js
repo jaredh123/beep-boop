4f36f1946188ecaed6a6c02187111151f448663b
jaredh123 <jared.hanson12345@gmail.com>
2019-01-25 10:10:40 -0800

    Add some functionality to input and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
+  $("#run").submit(function(event) {
+    event.preventDefault();
 
+    var number = parseInt($("#input").val());
+    var b = [];
+    var c = [];
+
+
+
+//  "Beep!"
+//  "Boop!"
+//  "I'm sorry, Dave. I'm afraid I can't do that."
+
+
+    alert("submit");
+
+
+
+  
+    $("#result").show().text(number);
+  });
+
+
+
+
+
+
+  $("#clear").submit(function(event) {
+    event.preventDefault();
+
+    alert("clear");
+    $("#result").hide();
+  });
 });
