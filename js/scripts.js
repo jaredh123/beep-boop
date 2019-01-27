c172dfbb8ec8a26ce228f4fa7a4f96507d12e395
jaredh123 <jared.hanson12345@gmail.com>
2019-01-26 16:55:22 -0800

    fix all array substitutions.

--- js/scripts.js
+++ js/scripts.js
@@ -9,36 +9,26 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputNum = parseInt($("#input").val());
-    var searcher = [];
-    // var b = [];
-    // var c = [];
+
 
 
 // Creates array of numbers from 0 through input
     var numList = [];
     for (var i = 0; i <= inputNum; i++) {
-      numList.push(i);
-// HAL
-      if (numList[i] % 3 === 0 && numList[i] !== 0) {
-        numList.splice(i, 1, "HAL");
+
+      if (i % 3 === 0 && i !== 0) {
+        numList.push("HAL");
       }
 //--------------------------------------------------------------
-
-      else if (numList[i] % 3 !== 0 && numList[i] !== 0) {
-        searcher.push(i);
-        searcher.toString();
-        searcher.join('');
-        if (searcher.includes("1")) {
-          numList.splice(i, 1, "Boop!");
-          searcher.split(' ');
-          searcher.shift();
-          // }
-          // else {
-          //   searcher.split(" ");
-          //   searcher.shift();
-        }
+      else if ((i.toString().includes("1"))) {
+        numList.push("1");
       }
-
+      else if ((i.toString().includes("0"))) {
+        numList.push("0");
+      }
+      else {
+        numList.push(i);
+      }
 //--------------------------------------------------------------
     }
 //--------------------------------------------------------------
@@ -52,29 +42,13 @@ $(document).ready(function() {
 
 
 
-
-
-
-
-
     alert(numList.length);
-    alert(searcher.length);
+  //  alert(searcher.length);
     $("#result").show().text(numList);
   });
+
   $("#clear").submit(function(event) {
     event.preventDefault();
     $("#result").hide();
   });
 });
-
-
-
-
-// for (var j = 0; j <= inputNum; j++) {
-//   for (var k = 0; k <= numList.length; k++) {
-//     if (numList[k] % j === 0 && numList[k] !== j) {
-//       numlist.splice(k, 1, "text");
-//       k--;
-//     }
-//   }
-// }
